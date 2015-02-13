5fbca43f4630892fab000b697d92ea2a78ce53e7
lisa.lau07@gmail.com <lisa.lau07@gmail.com>
2015-02-13 15:31:10 -0500

    Texture loading & behaviour control panel control

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -16,5 +16,56 @@ public class CharacterFocusChangeEvent : IEvent
         _tag = tag;
     }
 
-    public void LogEvent() { LogManager.Instance.Log("Input event"); }
+    public void LogEvent() { LogManager.Instance.Log("Input Event"); }
+}
+
+public class ThoughtChangeEvent  : IEvent
+{
+    private Actor _actorTarget;
+    private Thought _thought;
+
+    public Thought Thought { get { return _thought; } }
+    public Actor ActorTarget { get { return _actorTarget; } }
+
+    public ThoughtChangeEvent(Thought thought, Actor target)
+    {
+        _actorTarget = target;
+        _thought = thought;
+    }
+    
+    public void LogEvent() { LogManager.Instance.Log("Thought Change Event"); }
+}
+
+public class HealthChangeEvent : IEvent
+{
+    private Actor _actorTarget;
+    private int _amount;
+
+    public int Amount { get { return _amount; } }
+    public Actor ActorTarget { get { return _actorTarget; } }
+
+    public HealthChangeEvent(int amount, Actor target)
+    {
+        _actorTarget = target;
+        _amount = amount;
+    }
+
+    public void LogEvent() { LogManager.Instance.Log("Health Change Event"); }
+}
+
+public class StatusChangeEvent : IEvent
+{
+    private Actor _actorTarget;
+    private string _status;
+
+    public string Status{ get { return _status; } }
+    public Actor ActorTarget { get { return _actorTarget; } }
+
+    public StatusChangeEvent(string status, Actor target)
+    {
+        _actorTarget = target;
+        _status = status;
+    }
+
+    public void LogEvent() { LogManager.Instance.Log("Status Change Event"); }
 }
